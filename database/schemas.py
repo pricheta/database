365f2f1668cd2681175713cc2993ba96ff62aa77
pricheta <pricheta@inbox.ru>
2025-06-09 20:29:52 +0300

    Описана базовая схема

---
+++ database/schemas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
