15c728431764b8101ef6c7c63383e019080e1b5a
pricheta <pricheta@inbox.ru>
2025-06-09 20:27:41 +0300

    Описан движок

---
+++ database/engine.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from config import db_settings
+
+
+DATABASE_URL = db_settings.get_db_url()
+
+
+engine = create_async_engine(url=DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
