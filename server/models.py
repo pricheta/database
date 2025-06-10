b2b49a0187e821c1b1745b3763c67461d9e30ec0
pricheta <pricheta@inbox.ru>
2025-06-10 19:38:16 +0300

    Хэширование пароля

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, model_validator
 
 from database.hash import pwd_context
 
@@ -9,7 +9,8 @@ class CreateUserRequest(BaseModel):
     password: str
 
 
-    @classmethod
-    @field_validator("password")
-    def hash_password(cls, v: str) -> str:
-        return pwd_context.hash(v)
+    @model_validator(mode="after")
+    def hash_password(self) -> "CreateUserRequest":
+        if not self.password.startswith("$2b$"):
+            self.password = pwd_context.hash(self.password)
+        return self
