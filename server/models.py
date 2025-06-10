241add65abd6a108eec940bd2369780ac45154b3
pricheta <pricheta@inbox.ru>
2025-06-10 19:30:21 +0300

    Хэширование пароля

--- server/models.py
+++ server/models.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from database.hash import pwd_context
 
 
 class CreateUserRequest(BaseModel):
     email: str
     name: str
     password: str
+
+
+    @classmethod
+    @field_validator("password")
+    def hash_password(cls, v: str) -> str:
+        return pwd_context.hash(v)
